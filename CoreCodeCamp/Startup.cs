44b4ab911ae1ef35c5d2027813178c1187caed00
osman-sahin <osmansahin.eng@gmail.com>
2020-05-26 02:19:12 +0300

    API Versioning, Functional API reloadconfig

--- CoreCodeCamp/Startup.cs
+++ CoreCodeCamp/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,7 +25,21 @@ namespace CoreCodeCamp
 
             services.AddAutoMapper();
 
-            services.AddMvc()
+            services.AddApiVersioning(opt =>
+            {
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 1);
+                opt.ReportApiVersions = true;
+                //opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+                //opt.ApiVersionReader = new QueryStringApiVersionReader("v");
+                // versioning by namespaces, content type, writing your own reader, write your own resolvers 
+
+                opt.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("X-Version"),
+                    new QueryStringApiVersionReader("ver", "version"));
+            });
+
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
               .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
